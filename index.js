82a894ca2c648a1c880fbea2fcebcaaf14051ea3
şöhrett <sohretturaman@gmail.com>
2024-02-29 12:10:56 +0300

    structure of node project has been created for note app

---
+++ index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+//The contents of the index.js file used for starting the application gets simplified as follows:
+const app = require("./app"); // the actual Express application
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
